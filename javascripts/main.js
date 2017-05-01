32bf91ca4b42a7f4d9081954c1e5721e516fe25e
Adam Reidelbach <adamreidelbach@gmail.com>
2017-05-01 13:43:22 -0500

    working on theme changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
 console.log("testing main.js");
+
+//** AR - When the user clicks on the dark theme checkbox, change the background color of your application to a dark gray, and the font color for messages should be white(ish)... you pick.
+
+var darkTheme = document.getElementById("darkTheme");
+var largeText = document.getElementById("largeText");
+
+darkTheme.addEventListener("click", function() {
+
+})
+
+//** AR
