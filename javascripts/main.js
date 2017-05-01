6220961f53639ce967166e9f6f32249c23a3b4c1
Tamela Lerma <tlerma83@gmail.com>
2017-05-01 13:25:56 -0500

    commented out js in main js that is not yet functional

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,27 +1,27 @@
 console.log("testing main.js");
 
-var holdClickedDiv;
 
 
-funcName.methodName();
-
-var clickedDeleteButton = document.getElementsByClassName("whateverClassNameis").addEventListener("click", deleteOutput);
-
-
-function deleteOutput () {
-    holdClickedDiv.event.currentTarget;
-    var removeButton =
-    for (var d = 0; 0 < holdMyVar.length; d++) {
-        holdMyVar
-    }
-}
-
-    CarLot.addStyle(holyCrapOnAStick, "red");
-    //this sends current target and a color to change element
-}
-
-//get the variable that is holding which event was selected, get the p tag and it is an array so tell it which index you want. I need the last p tag which has an index 0f 3
-function changeText () {
-    var holdMyCrap = holyCrapOnAStick.getElementsByTagName("p")[3];
-    console.log("what is this", holdMyCrap);
-    holdMyCrap.innerHTML = inputField.value;
+////testing out some stuf TL
+//var holdClickedDiv;
+//
+//
+//funcName.methodName();
+//
+//var clickedDeleteButton = document.getElementsByClassName("whateverClassNameis").addEventListener("click", deleteOutput);
+//
+//
+//function deleteOutput () {
+//    holdClickedDiv.event.currentTarget;
+//    var removeButton =
+//    for (var d = 0; 0 < holdMyVar.length; d++) {
+//        holdMyVar
+//    }
+//}
+//
+//    CarLot.addStyle(holyCrapOnAStick, "red");
+//
+//function changeText () {
+//    var holdMyCrap = holyCrapOnAStick.getElementsByTagName("p")[3];
+//    console.log("what is this", holdMyCrap);
+//    holdMyCrap.innerHTML = inputField.value;
