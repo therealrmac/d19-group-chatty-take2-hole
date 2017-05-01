8ed0c231b0a0f5b00fbe899c856118804a4e1ee6
n.herron <nherron@nherrons-MacBook-Pro.local>
2017-05-01 12:54:51 -0500

    load.js IIFE loading the json file

--- javascripts/load.js
+++ javascripts/load.js
@@ -1,1 +1,21 @@
 console.log("testing load.js");
+
+var Chatty = (function(){
+
+	var messages = [];
+
+	return {
+		loadMessages: function (callBack){
+			var loader = new XMLHttpRequest();
+      		loader.open("GET", "messages.json");
+      		loader.send();
+      		loader.addEventListener("load", function (){
+      			messages = JSON.parse(this.responseText);
+      			invoke(messages);
+      		});
+		}
+
+	}
+
+
+})();
