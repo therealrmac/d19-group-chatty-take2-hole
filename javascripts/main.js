5fd07c4adae4102b1cf173b51a7f235d9f789876
Tamela Lerma <tlerma83@gmail.com>
2017-05-03 17:53:52 -0500

    worked on main styling and added BTN styling for DarkTheme, added css, js on input.js, and js on main.js under DarkTheme toggle function

--- javascripts/main.js
+++ javascripts/main.js
@@ -61,8 +61,21 @@ var messageArea = document.getElementById("messageArea");
 
 darkTheme.addEventListener("click", function() {
 	var themeVal = darkTheme.value;
+    var butts = document.getElementsByTagName("button");
+
 	if (themeVal == 1) {
-		event.target.closest("body").classList.toggle("ar-bodyStyle");
+        event.target.closest("body").classList.toggle("ar-bodyStyle");
+        //adding background color, border,  and font color to clear Message BTN
+        var messageButt = document.getElementsByClassName("tl-message-btn");
+        messageButt[0].classList.toggle("tl-message-btn-toggle");
+
+        //adding background color, border,  and font color to clear input field, toggling same class as before
+        input.classList.toggle("tl-message-btn-toggle");
+
+        //adding background color, border,  and font color to all BTNs starting at index 1, toggling same class as before
+        for (var p = 1; p < butts.length; p++) {
+            butts[p].classList.toggle("tl-button");
+        }
         // event.target.getTagName("input").classList.toggle("buttonStyle");
 	}
 })
@@ -83,7 +96,7 @@ function jsonMessage (messages){
 	var divContent;
 	var text= messages;
 	for(var i=0; i< text.length; i++){
-		divContent= "<div>"+"<span>"+ text[i].text+"</span>"+ "<button class='delete'>" + 'Delete' + "</button>" + "<button>Edit</button>" +"<br>"+"</div>";
+		divContent= "<div class='tl-style-div'>"+"<span>"+ text[i].text+"</span><br>"+ "<button class='delete'>" + 'Delete' + "</button>" + "<button>Edit</button>" +"<br>"+"</div>";
 		output.innerHTML+= divContent;
 		console.log(divContent);
 	}
